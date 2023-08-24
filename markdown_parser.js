5750ceeb35a3296d1d8c172b787dd8713c0b7da8
Alexander_Kabui <alexanderkabua@gmail.com>
2023-08-25 01:45:31 +0300

    Add code highlighting functionality

---
+++ markdown_parser.js
@@ -0,0 +1,35 @@
+//parse for converting markdown to html
+
+
+
+ function  configureMarkdownParser() { 
+ 	//add code highlighting functionality
+   const {markedHighlight} = globalThis.markedHighlight;
+   let new_marked = new marked.Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+)
+     // Set options
+  new_marked.use({
+    pedantic: false,
+    gfm: true,
+  });
+
+
+   return new_marked
+}
+
+
+function updatePreview(){
+   previewContent = document.querySelector("#output");
+  var markdownContent = editor.getValue();
+  var htmlContent = configureMarkdownParser().parse(markdownContent)
+  previewContent.innerHTML = htmlContent;
+}
+
+

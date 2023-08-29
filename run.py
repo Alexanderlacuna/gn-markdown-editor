f61e2f98a65b9192f63f21e70c03a2c2adb5b67e
Alexander_Kabui <alexanderkabua@gmail.com>
2023-08-29 21:52:37 +0300

    get diff for files

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # requirements are markdown,flask and python
-from flask import Flask, request, render_template,redirect,url_for
+from flask import Flask, request, render_template, redirect, url_for
 from github import Github
 from urllib.parse import urlparse
 from github import InputGitTreeElement
@@ -15,21 +15,23 @@ def authenticator(f):
         return f(*args, **kwargs)
     return decorated_function
 
+
 def gn_auth_access():
-    # the idea for this is to try to authenticate using gn 
-    #auth if success give users gn access token to  commit
+    # the idea for this is to try to authenticate using gn
+    # auth if success give users gn access token to  commit
     # check for gn authentication then get access token to repo
-    pass 
+    pass
+
 
 def parse_github_url(url):
     parsed_url = urlparse(url)
     path_parts = parsed_url.path.split("/")
-    
+
     owner = path_parts[1]
     repository_name = path_parts[2].split(".")[0]
     file_path = "/".join(path_parts[5:])
     branch = path_parts[4]
-    
+
     return {
         "owner": owner,
         "repository_name": repository_name,
@@ -37,10 +39,17 @@ def parse_github_url(url):
         "branch": branch
     }
 
+
+def git_diff(repo,branch,file_path,commit):
+    last_head = repo.get_branch(branch)
+    diff_url = repo.compare(last_head.commit.sha,
+                            commit.sha)
+    return diff_url.diff_url
+
 @app.route("/", methods=["GET","POST"])
 def edit_file_content():
 
-    parsed_data = request.json["git_url"]
+    parsed_data = parse_github_url(edit_file)
     repo_name = parsed_data.get("repository_name")
     file_path = parsed_data.get("file_path")
 
@@ -52,14 +61,19 @@ def edit_file_content():
         file_content = repo.get_contents(
             file_path).decoded_content.decode('utf-8')
 
-        return render_template("preview.html",data=file_content)
+        return render_template("preview.html", data=file_content)
     except Exception as e:
         # add error page
         return f"Error fetching file: {str(e)}"
 
-
 @app.route('/commit', methods=['POST'])
 def commit():
+
+    '''
+    route to commit changes for  an existing file or new file
+
+    '''
+
     if request.method == 'POST':
         parsed_data = request.json["git_url"]
         repo_name = parsed_data.get("repository_name")
@@ -74,14 +88,14 @@ def commit():
         # fix this below
         blob = repo.create_git_blob(results["new_changes"], 'utf-8')
         tree_elements = [
-            InputGitTreeElement(file_path,"100644", "blob",results["new_changes"])
+            InputGitTreeElement(file_path, "100644", "blob",
+                                results["new_changes"])
         ]
         new_tree = repo.create_git_tree(tree_elements, base_tree=master_tree)
-        commit = repo.create_git_commit(results["msg"], new_tree, [repo.get_git_commit(master_sha)])
+        commit = repo.create_git_commit(results["msg"], new_tree, [
+                                        repo.get_git_commit(master_sha)])
         master_ref.edit(commit.sha)
         return render_template("success.html", text="Successfully made changes")
-
-
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)

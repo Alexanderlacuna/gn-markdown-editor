7cae46c00ea63862f19978b42f841e35e4362f93
Alexander_Kabui <alexanderkabua@gmail.com>
2023-09-12 01:11:44 +0300

    update package definition

--- guix.scm
+++ guix.scm
@@ -2,36 +2,41 @@
 ;; WIP(Js packaging)
 
 
-(use-modules
- (gnu packages python)
- (gnu packages python-web)
- (gnu packages python-xyz)
- (guix build-system python)
- (guix git-download)
- (guix download)
- (guix licenses)
- (guix packages))
+(define-module (gn-markdown-editor)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system python)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages python-xyz)
+  #:use-module ((guix licenses) #:prefix license:))
 
-(define-public gn-markdown
-  (package
-    (name "gn-markdown")
-    (version "1.0")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (pypi-uri "prov" version))
-        (sha256
-          (base32
-            "1a9h406laclxalmdny37m0yyw7y17n359akclbahimdggq853jd0"))))
-    (build-system python-build-system)
-    (propagated-inputs
-     `(("python" ,python)
-       ("python-markdown" ,python-markdown)
-       ("python-pygithub" , python-pygithub)
-       ("python-flask" ,python-flask)))
-    (home-page "https://github.com/Alexanderlacuna/gn-markdown-editor")
-    (synopsis "Gn-markdown-editor is a tiny web tool to preview Markdown formatted text.")
-    (description "Gn-markdown-editor is a tiny web tool to preview Markdown formatted text.")
-    (license license:agpl3+)))
+;;redo
 
-;;(define-public python-xyz gn-markdown)
+(package
+  (name "gn-markdown-editor")
+  (version "0.1")
+  (source
+    (origin
+      (method git-fetch)
+      (uri (git-reference
+             (url "https://github.com/Alexanderlacuna/gn-markdown-editor.git")
+             (commit (string-append "v" version))))
+      (file-name (git-file-name name version))
+      (sha256
+       (base32
+        "11hfx5x3jg4hyfxzav6ypsb57mahb5nk6qzg4zn1pyy1lilllqj6"))))
+  (build-system python-build-system)
+  (arguments `(#:tests? #f))
+  (propagated-inputs (list python
+         python-markdown
+         python-pygithub
+         python-flask ))
+  (home-page "https://github.com/Alexanderlacuna/gn-markdown-editor")
+  (synopsis "Gn-markdown-editor is a tiny web tool to preview Markdown formatted text.")
+  (description "Gn-markdown-editor is a tiny web tool to preview Markdown formatted text.")
+  (license license:gpl3))
+

3b6ecac9997baae52297a3c5854895ef7d194e8d
Alexander_Kabui <alexanderkabua@gmail.com>
2023-09-11 08:52:26 +0300

    code refactoring

--- app/run.py
+++ app/run.py
@@ -19,8 +19,7 @@ def authenticator(f):
             return "Unauthorized: GitHub token not provided", 401
         try:
             g = Github(github_token)
-            user = g.get_user()
-            if user.name:
+            if g.get_user().name:
                 return f(g, *args, **kwargs)
         except GithubException as e:
             return f"GitHub authentication failed: {str(e)}", 401
@@ -34,25 +33,22 @@ def get_github_access_token():
 
 
 def parse_github_url(url):
-    parsed_url = urlparse(url)
-    path_parts = parsed_url.path.split("/")
-
-    owner = path_parts[1]
-    repository_name = path_parts[2].split(".")[0]
-    file_path = "/".join(path_parts[5:])
-    branch = path_parts[4]
-
+    """require format for this 
+    https://github.com/{username}/{repo_name}/blob/{branch}/README.md
+    """
+    path_parts = urlparse(url).path.split("/")
     return {
-        "owner": owner,
-        "repository_name": repository_name,
-        "file_path": file_path,
-        "branch": branch
+        "owner": path_parts[1],
+        "repository_name": path_parts[2].split(".")[0],
+        "file_path": "/".join(path_parts[5:]),
+        "branch":  path_parts[4]
     }
 
 
 def git_diff(repo, branch, file_path, commit):
-    last_head = repo.get_branch(branch)
-    diff_url = repo.compare(last_head.commit.sha,
+    """function to equivalent to git diff
+    """
+    diff_url = repo.compare(repo.get_branch(branch).commit.sha,
                             commit.sha)
     return diff_url.diff_url
 
@@ -64,12 +60,10 @@ def edit_file_content(g):
     if not edit_file:
         edit_file = "https://github.com/Alexanderlacuna/data-vault-2/blob/master/README.md"
     parsed_data = parse_github_url(edit_file)
-    repo_name = parsed_data.get("repository_name")
-    file_path = parsed_data.get("file_path")
-    repo = g.get_user().get_repo(repo_name)
+    repo = g.get_user().get_repo(parsed_data.get("repository_name"))
     try:
         file_content = repo.get_contents(
-            file_path).decoded_content.decode('utf-8')
+            parsed_data.get("file_path")).decoded_content.decode('utf-8')
         return render_template("preview.html", data=file_content, refresh_link=edit_file)
     except Exception as e:
         return f"Error fetching file: {str(e)}"
@@ -84,24 +78,27 @@ def commit(g):
     request.json["git_url"] = "https://github.com/Alexanderlacuna/data-vault-2/blob/master/README.md"
     if request.method == 'POST':
         parsed_data = parse_github_url(request.json["git_url"])
-        repo_name = parsed_data.get("repository_name")
-        file_path = parsed_data.get("file_path")
-        repo = g.get_user().get_repo(repo_name)
+        repo = g.get_user().get_repo(parsed_data.get("repository_name"))
         results = request.json
         master_ref = repo.get_git_ref('heads/master')
         master_sha = master_ref.object.sha
         master_tree = repo.get_git_tree(master_sha)
         # fix this below
-        blob = repo.create_git_blob(results["new_changes"], 'utf-8')
-        tree_elements = [
-            InputGitTreeElement(file_path, "100644", "blob",
-                                results["new_changes"])
-        ]
-        new_tree = repo.create_git_tree(tree_elements, base_tree=master_tree)
-        commit = repo.create_git_commit(results["msg"], new_tree, [
-                                        repo.get_git_commit(master_sha)])
-        master_ref.edit(commit.sha)
-        return jsonify({"commit": commit.sha, "commit_message": results["msg"]})
+        try:
+
+            blob = repo.create_git_blob(results["new_changes"], 'utf-8')
+            tree_elements = [
+                InputGitTreeElement(parsed_data.get("file_path"), "100644", "blob",
+                                    results["new_changes"])
+            ]
+            new_tree = repo.create_git_tree(
+                tree_elements, base_tree=master_tree)
+            commit = repo.create_git_commit(results["msg"], new_tree, [
+                                            repo.get_git_commit(master_sha)])
+            master_ref.edit(commit.sha)
+            return jsonify({"commit": commit.sha, "commit_message": results["msg"]})
+        except Exception as error:
+            return jsonify({"error": str(error)})
 
 
 @app.route('/parser', methods=['POST'])
@@ -114,5 +111,4 @@ def marked_down_parser():
             request.json["text"], extensions=["tables"])
         return jsonify({"data": results})
     except Exception as e:
-        raise e
         return f"error while parsing markdown  {str(e)}"

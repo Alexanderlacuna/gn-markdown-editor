4694fd067c38ddf5d2f9f6f1a0e74c17728cf962
Alexander_Kabui <alexanderkabua@gmail.com>
2023-09-11 09:38:25 +0300

    update config file

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class DefaultConfig:
     PORT = 5000
     DEBUG = True
     SECRET_KEY = 'your_secret_key_here'
-    GITHUB_ACCESS_TOKEN = 'your_github_access_token_here'  # Add your GitHub access token here
+    GITHUB_ACCESS_TOKEN = ''  # Add your GitHub access token here
     BASE_REPO = None #optional value for base repo
+    BASE_BRANCH = "master"
 
